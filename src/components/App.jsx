9f082f51962f6c09c0c93c055204c65f7c09fde7
Manuel Pérez <manuel.perez.pozo@gmail.com>
2021-10-10 13:18:08 -0300

    initial setup

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../styles/components/App.css';
+
+const App = ()=>{
+    return(<h1>Hola mundo desde mi bitch</h1>)
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use:[
+            {
+            loader: MiniCssExtractPlugin.loader
+            },
+            'css-loader'
+        ]
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+        filename: "assets/[name].css",
+    }),
+  ],
+  mode: mode,
+  devServer: {
+    static: {
+        directory: path.join(__dirname, "./dist"),
+      },
+    compress: true,
+    port: 3000,
+  },
+};
